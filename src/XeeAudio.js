7b2f92d6fe1ce370331aa2f4c7f071227b0e34a5
louie <hzxiezhongzheng@corp.netease.com>
2017-05-10 20:23:18 +0800

    build

---
+++ src/XeeAudio.js
@@ -0,0 +1,40 @@
+import jQuery from 'jquery';
+
+import XeeMedia from './XeeMedia';
+
+const $ = jQuery;
+
+class XeeAudio extends XeeMedia {
+
+    constructor (opt) {
+        super(opt)
+
+        if (opt.ele) {
+            this.$media = $(opt.ele);
+        } else {
+            this.$media = $(`<audio src="${opt.src}">`);
+            this.__getWrapper__().append(this.$media);
+        }
+
+        XeeMedia.IS_SUPPORT_MEDIA && this.__init__();
+    }
+
+    __getWrapper__ () {
+        let $wrapper = $('.myAudio--wrapper');
+
+        if (!$wrapper[0]) {
+            $wrapper = $('<div>').addClass('myAudio--wrapper').css({
+                display: 'none'
+            }).appendTo('body');
+        }
+
+        return $wrapper;
+    }
+
+}
+
+XeeAudio.IS_SUPPORT_MEDIA = XeeMedia.IS_SUPPORT_MEDIA;
+XeeAudio.PLAY_THROUGH = XeeMedia.PLAY_THROUGH;
+XeeAudio.CAN_PLAY = XeeMedia.CAN_PLAY;
+
+export default XeeAudio;
